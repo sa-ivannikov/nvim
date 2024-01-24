e7480e7f8a39515f7b2446f9e609508e7f0d6e46
Sergey Ivannikov <sergey@siv-mba.local>
2024-01-24 21:01:57 +0400

    Add treesj plugin

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,12 @@
+return {
+	"Wansmer/treesj",
+	keys = {
+		"<leader>m",
+	},
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("treesj").setup({})
+	end,
+}
