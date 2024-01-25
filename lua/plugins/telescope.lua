ff2e83e0fc18881cda29794868ee39a25215009a
Sergey Ivannikov <sergey@siv-mba.local>
2024-01-25 16:57:46 +0400

    add dynamic workspace symbols to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,42 +1,44 @@
 return {
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        config = function()
-            local telescope = require("telescope")
-            local builtin = require("telescope.builtin")
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
 
-            telescope.setup({
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<C-h>"] = "which_key",
-                            ["<C-j>"] = "move_selection_next",
-                            ["<C-k>"] = "move_selection_previous",
-                        }
-                    }
-                },
-                pickers = {
-                },
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({})
-                    }
-                }
-            })
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-h>"] = "which_key",
+							["<C-j>"] = "move_selection_next",
+							["<C-k>"] = "move_selection_previous",
+						},
+					},
+				},
+				pickers = {},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>b", builtin.buffers, {})
-            vim.keymap.set("n", "<leader>g", builtin.grep_string, {})
-            vim.keymap.set("v", "<leader>g", builtin.grep_string, {})
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>g", builtin.grep_string, {})
+			vim.keymap.set("v", "<leader>g", builtin.grep_string, {})
+			vim.keymap.set("n", "<leader>fs", function()
+				builtin.lsp_dynamic_workspace_symbols()
+			end, {})
 
-            telescope.load_extension("ui-select")
-        end,
-    }
+			telescope.load_extension("ui-select")
+		end,
+	},
 }
