a873928831240fef6a1b8b3f7377a023408b9ec6
Sergey Ivannikov <sergey@siv-mba.local>
2024-01-25 16:57:16 +0400

    change HK for completion invocation

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -28,7 +28,7 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-Tab>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
                     ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
@@ -36,7 +36,7 @@ return {
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					-- { name = "luasnip" },
+					{ name = "luasnip" },
 				}, {
 					{ name = "buffer" },
 				}),
