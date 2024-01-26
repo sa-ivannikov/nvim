3ed08b3c4b60a41b7b2b9f61e0ba45d2cc1586b0
Sergey Ivannikov <sergey@siv-mba.local>
2024-01-26 15:56:54 +0400

    add bufferline plugin and buf switch keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,9 @@
+local opts = { noremap = true, silent = true }
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<C-x>", ":bd<CR>", opts)
+vim.keymap.set("n", "<C-x>q", ":bd!<CR>", opts)
