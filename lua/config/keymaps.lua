ae231ce4e5417429b2ed6042c59210fee41d6b65
Sergey Ivannikov <sergey@siv-mba.local>
2024-01-29 13:16:29 +0400

    Change bufclose mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,5 +5,5 @@ vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
-vim.keymap.set("n", "<C-x>", ":bd<CR>", opts)
-vim.keymap.set("n", "<C-x>q", ":bd!<CR>", opts)
+vim.keymap.set("n", "<Leader>xx", ":bd<CR>", opts)
+vim.keymap.set("n", "<Leader>XX", ":bd!<CR>", opts)
