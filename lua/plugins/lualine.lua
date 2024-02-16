f8d51d84a0e7c23dab20d7793525dea08623d0ce
Sergey Ivannikov <sergey@siv-mba.local>
2024-02-16 15:12:06 +0400

    Add full relative path to lualine filename section

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,15 @@
+local function relfp()
+	return vim.fn.expand("%")
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup({
+			sections = {
+				lualine_c = { relfp },
+			},
 		})
 	end,
 }
